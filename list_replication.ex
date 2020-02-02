52ed866ed2750de7eb7ff3e63d8dd11d54a4d899
Benjamin Manford <benjaminmanford@gmail.com>
2020-02-02 16:54:20 +0000

    Add list replication solution

---
+++ list_replication.ex
@@ -0,0 +1,15 @@
+defmodule Solution do
+    def read_input() do
+        IO.read(:all)
+        |> String.split([" ", "\n"])
+    end
+
+    def replicate() do
+        [count | list] = read_input()
+        {count, _} = Integer.parse(count)
+
+        Enum.map(list, fn n -> for _ <- 1..count, do: IO.puts(n) end)
+    end
+end
+
+Solution.replicate()
