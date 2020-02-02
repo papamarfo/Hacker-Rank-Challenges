a8d88422e97fc52f19d13df4d4240c838d1a2087
Benjamin Manford <benjaminmanford@gmail.com>
2020-02-02 15:22:46 +0000

    Add hello world n times solution

---
+++ hello_world_n_times.ex
@@ -0,0 +1,17 @@
+defmodule Solution do
+    def read_input() do
+        IO.read(:line)
+        |> String.trim()
+        |> String.to_integer()
+    end
+
+    def message() do
+        n = read_input()
+
+        for _n <- 1..n do
+            IO.puts "Hello World"
+        end
+    end
+end
+
+Solution.message()
